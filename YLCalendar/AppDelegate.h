ea64bd6d29fb606b40bfc0bd08e23c5e4e799ac2
admin <ylgjceo@gmail.com>
2016-08-30 11:37:28 -0700

    初始

---
+++ YLCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLCalendar
+//
+//  Created by admin on 16/8/30.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLCalendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLCalendar
+//
+//  Created by admin on 16/8/30.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
